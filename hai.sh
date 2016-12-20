26cdd0bccb84af879f7f5e7c5c1335951b8143c4
Nirmal RajKumar <nirmalrajkumar.b@asmltd.com>
2016-12-20 16:42:19 +0530

    done

--- hai.sh
+++ hai.sh
@@ -1,2 +1,13 @@
-find /home/asm/Documents/test5/hai.sh -type d -ctime +5 -exec rm -rf {} \;
-
+del_file()
+for entry in `ls $prga`
+do
+    echo $entry
+    var=$entry
+    find $var -mtime +2 -type f -delete
+done
+echo 'enter a value'
+read val
+if [ "$val" -ge "2" ]
+then
+    del_file
+fi
